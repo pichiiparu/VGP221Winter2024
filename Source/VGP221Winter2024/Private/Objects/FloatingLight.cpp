f95068a6308700ad2415ce551c212f60ac47a66c
Rae Leda <veronica.rleda@gmail.com>
2024-02-07 01:38:01 -0500

    Updated Landscape

--- Source/VGP221Winter2024/Private/Objects/FloatingLight.cpp
+++ Source/VGP221Winter2024/Private/Objects/FloatingLight.cpp
@@ -7,10 +7,10 @@ AFloatingLight::AFloatingLight()
     PrimaryActorTick.bCanEverTick = true;
 
     LightComponent = CreateDefaultSubobject<UPointLightComponent>(TEXT("Point Light")); // Create PointLightComponent
-    RootComponent = LightComponent; // Attach PointLightComponent to root
-
+    //RootComponent = LightComponent; // Attach PointLightComponent to root
+    LightComponent->SetupAttachment(RootComponent); 
+    LightComponent->SetMobility(EComponentMobility::Movable); 
     static ConstructorHelpers::FObjectFinder<UPointLightComponent> PointLightAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube")); 
-    LightComponent->SetMobility(EComponentMobility::Movable);
     LightComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f)); 
 }
 
@@ -29,10 +29,17 @@ void AFloatingLight::Tick(float DeltaTime)
     FRotator NewRotation = GetActorRotation(); 
     float RunningTime = GetGameTimeSinceCreation(); 
     float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime)); 
-    NewLocation.Z += DeltaHeight * 20.0f;        //Scale our height by a factor of 20
+    NewLocation.Z += DeltaHeight * 50.0f;        //Scale our height by a factor of 20
     float DeltaRotation = DeltaTime * 20.0f;    //Rotate by 20 degrees per second
     NewRotation.Yaw += DeltaRotation; 
     SetActorLocationAndRotation(NewLocation, NewRotation); 
+
+    float frequency = 0.5f; 
+    // Calculate the brightness based on a sine function to make it fade and go bright
+    float Brightness = FMath::Abs(FMath::Sin((RunningTime + DeltaTime) * frequency));
+    Brightness *= 100000.0f; 
+    // Set the brightness of the light component
+    LightComponent->SetIntensity(Brightness);
 
     RunningTime += DeltaTime;
 }
